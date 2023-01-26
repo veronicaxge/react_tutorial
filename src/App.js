db1692a5cbb9dc679438caa5d24628b981d5db5e
Veronica Ge <veronicaxge@gmail.com>
2023-01-25 22:12:17 -0500

    learns about props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// Create a square component. Indicate `value` as the property that a square can take.
+function Square({ value }){ //need to put `value` in {} 
+
+  function handleClick(){
+    console.log("Clicked!")
+  }
+
+  return (
+    <button 
+      className="square"
+      onClick = {handleClick}
+    >
+      {value} 
+    </button>); //need to put `value` in {} 
+}
+
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value = "1" />  {/* pass a value property to each square */}
+        <Square value = "2" />
+        <Square value = "3" />
+      </div>
+      <div className="board-row">
+        <Square value = "4" />
+        <Square value = "5" />
+        <Square value = "6" />
+      </div>
+      <div className="board-row">
+        <Square value = "7" />
+        <Square value = "8" />
+        <Square value = "9" />
+      </div>
+    </>
+  );
 }
