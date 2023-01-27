57998b058ed01aea3bfdcb258f87c8ebd8bd0b98
Veronica Ge <veronicaxge@gmail.com>
2023-01-27 14:30:37 -0500

    lifting state from child component Square into parent component Board

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
 import { useState } from "react"; //`useState` function to tell components to "remember" things. 
 
-// Create a square component. Indicate `value` as the property that a square can take.
-function Square(){ 
-
-  const[value, setValue] = useState(null);
-
-  function handleClick(){
-    setValue('X');
-  }
-
-  return (
-    <button 
-      className="square"
-      onClick = {handleClick}
-    >
-      {value} 
-    </button>); //need to put `value` in {} 
-}
-
-
 export default function Board() {
+  const[squares, setSquares] = useState(Array(9).fill(null)) //lifting state from Square to parent component Board 
+
   return (
     <>
       <div className="board-row">
-        <Square />  {/* pass a value property to each square */}
-        <Square />
-        <Square />
+        <Square value = {squares[0]} />  {/* Board passes down a value prop to display in each Square */}
+        <Square value = {squares[1]} />
+        <Square value = {squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value = {squares[3]} />
+        <Square value = {squares[4]} />
+        <Square value = {squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value = {squares[6]} />
+        <Square value = {squares[7]} />
+        <Square value = {squares[8]} />
       </div>
     </>
   );
+}
+
+// Create a square component. Indicate `value` as the property that a square can take.
+function Square( {value} ){ 
+  return <button className="square"> {value} </button>; //need to put `value` in {} 
 }
