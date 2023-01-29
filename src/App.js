fa02aec2437e092025f678bcee40680c0b3abb75
Veronica Ge <veronicaxge@gmail.com>
2023-01-29 11:00:11 -0500

    decided the winner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"; //`useState` function to tell components to "remember" things. 
+import { useState } from 'react'; //`useState` function to tell components to "remember" things. 
 
 export default function Board() {
   //create a constant with the name `squares`. Initialize the value of the constant as an array of 9 with null. This constant's value can be set to another if certain event happens. 
@@ -7,8 +7,8 @@ export default function Board() {
   
 
   function handleClick(i){
-    if(squares[i]){
-      return;
+    if(squares[i] || calculateWinner(squares)){
+      return; //stop running the remaining function if the square is occupied or if a winner is decided
     }
 
     const nextSquares = squares.slice(); //make a copy of the squares array
@@ -23,8 +23,18 @@ export default function Board() {
     setXIsNext(!xIsNext) //flip the state of xIsNext from true to false, and vice versa. 
   }
 
+
+  const winner = calculateWinner(squares);
+  let status; //using let to restrict the variable to local scope
+  if (winner){
+    status = "Winner: " + winner;
+  } else{
+    status = "Next Player: " + (xIsNext? "X":"O");
+  }
+
   return (
-    <>
+    <div>
+      <div>{status}</div> {/* isn't status a property? why don't we need to define the property in Board function declaration?  */}
       <div className="board-row">
         <Square value = {squares[0]} onSquareClick = {() => handleClick(0)} />  {/* Board passes down a value prop to display in each Square */}
         <Square value = {squares[1]} onSquareClick = {() => handleClick(1)} />
@@ -40,7 +50,7 @@ export default function Board() {
         <Square value = {squares[7]} onSquareClick = {() => handleClick(7)} />
         <Square value = {squares[8]} onSquareClick = {() => handleClick(8)} />
       </div>
-    </>
+    </div>
   );
 }
 
@@ -51,6 +61,31 @@ function Square( {value, onSquareClick} ){
       {value} 
     </button>
   ); //need to put `value` in {} 
+}
+
+//create a function to decide the winner
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]  
+  ];
+
+  for(let i=0; i<lines.length; i++){ //need to write `let`; need ot use semicolons
+    const[a,b,c] = lines[i] 
+
+    if(squares[a] && squares[a] == squares[b] && squares[b] == squares[c]){
+      return squares[a]
+    }
+  };
+
+  return null;
 }
 
 
